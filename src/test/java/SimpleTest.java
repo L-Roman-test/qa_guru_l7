d5f70ce2d158b09f1ff88230deb53170c68b0154
roman.lelikov <roman.lelikov@brandmaker.com>
2021-02-19 20:24:42 +0700

    simpleTest for jenkins job

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SimpleTest {
+    int a = 5;
+    int b = 2;
+
+    @Test
+    @Tag("positive")
+    @DisplayName("Positive test")
+    public void multiplication() {
+        step("Сheck that multiplication is performed correctly", () -> {
+            int c = a * b;
+            assertEquals(10, c);
+        });
+    }
+
+
 }
